5e772f8e324ed3a117f9baa5237bbc0d91e7166b
Tony Padilla <vainehertz@yahoo.com>
2022-02-06 19:29:49 -0800

    Updates

---
+++ precache-manifest.765e7c232c8ae884fec6f8b6bc11d486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52138c14815135fabdca7d2327003b88",
+    "url": "/krab-kingdom/index.html"
+  },
+  {
+    "revision": "9ff67190ba5b9fbff82b",
+    "url": "/krab-kingdom/static/css/main.a3d7745c.chunk.css"
+  },
+  {
+    "revision": "8f74189ec1053b395df8",
+    "url": "/krab-kingdom/static/js/2.0851b246.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "/krab-kingdom/static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "9ff67190ba5b9fbff82b",
+    "url": "/krab-kingdom/static/js/main.d05bba8f.chunk.js"
+  },
+  {
+    "revision": "a0fccfc08c695f22c517",
+    "url": "/krab-kingdom/static/js/runtime~main.c0f93584.js"
+  },
+  {
+    "revision": "98706a4b18118491bbf3b80cb4552e03",
+    "url": "/krab-kingdom/static/media/OpenDyslexic-Bold.98706a4b.otf"
+  }
+]);
