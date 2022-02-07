c2c5a052c5c22308f299e270653cda429e19aacb
Tony Padilla <vainehertz@yahoo.com>
2022-02-06 21:16:29 -0800

    crab image added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 import './style.css';
 import './fonts/open-dyslexic/OpenDyslexic-Bold.otf';
+import crab from './ART/crab.png';
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -27,7 +28,7 @@ function useWindowDimensions() {
 }
 
 function App() {
-  const { height, width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
   let canvas;
   const canvasRef = useRef(null);
   useEffect(() => {
@@ -36,10 +37,16 @@ function App() {
     let canvasScale = width/1280;
     canvas.width = 1280 * canvasScale;
     canvas.height = 720 * canvasScale;
-    console.log(canvas.width + "height: " + canvas.height);
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'rgb(15, 15, 15)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-   }, []);
+    console.log(crab);
+    var crabImage = new Image();
+    crabImage.src = crab;
+    crabImage.onload = function(){
+    ctx.drawImage(crabImage,0,0,120,78);
+    }
+  }, []);
+
 
   return (
     <div className="App">
@@ -53,14 +60,16 @@ function App() {
       <canvas ref={canvasRef}/>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src="modal.js"></script>
-
+      <div className='center'>
       <div className="buttonsBorder">
-        <button className="newGame">New Game</button>
+        <button className="newGame">Start</button>
         <button className="info">Info</button>
+      </div>
       </div>
 	  </body>
   </div>
   );
+  
 }
 
 export default App;
